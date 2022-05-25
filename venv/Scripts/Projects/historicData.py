c228a35bfe160d48d87862720f71f00afee753de
mijos52 <styleethnic1@gmail.com>
2022-05-25 12:48:11 +0530

    candle data restriction

--- venv/Scripts/Projects/historicData.py
+++ venv/Scripts/Projects/historicData.py
@@ -10,11 +10,14 @@ headers = {
 
 # Get todays date and convert it into string
 # day = (str(date.today()))
-day = "2022-05-01"
+from_day = "2020-03-01"
+to_day = "2020-03-25"
 
+
+'''intrady candle data is limited to time apis dose not provide full data but could be used to make scanner '''
 
 r = requests.get('https://api.stocknote.com/intraday/candleData', params={
-  'symbolName': ['WIPRO'],  'fromDate': '%s 09:15:00'%day , 'interval': '1' ,
+  'symbolName': ['WIPRO'],  'fromDate': '%s 09:15:00'%from_day , 'toDate': '%s 09:15:00'%to_day , 'interval': '5' ,
 }, headers = headers)
 
 pprint (r.json())
